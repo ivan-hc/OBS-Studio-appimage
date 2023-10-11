b8212bb8955e33b43c5a57a624746f8305d972f1
小鸡 <345865759@163.com>
2023-10-11 15:01:46 +0800

    Update obs-junest.sh
    added more language display support

--- obs-junest.sh
+++ obs-junest.sh
@@ -101,7 +101,7 @@ export UNION_PRELOAD=$HERE
 export JUNEST_HOME=$HERE/.junest
 export PATH=$HERE/.local/share/junest/bin/:$PATH
 mkdir -p $HOME/.cache
-$HERE/.local/share/junest/bin/junest proot -n -b "--bind=/home --bind=/home/$(echo $USER) --bind=/media --bind=/mnt --bind=/opt --bind=/usr/lib/locale --bind=/etc/fonts" 2> /dev/null -- BINARY "$@"
+$HERE/.local/share/junest/bin/junest proot -n -b "--bind=/home --bind=/home/$(echo $USER) --bind=/media --bind=/mnt --bind=/opt --bind=/usr/lib/locale --bind=/etc/fonts --bind=/usr/share/fonts" 2> /dev/null -- BINARY "$@"
 EOF
 chmod a+x ./$APP.AppDir/AppRun
 sed -i "s#BINARY#$BIN#g" ./$APP.AppDir/AppRun
