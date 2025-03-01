540231fd2edce1fe837b6ffdc0468a1c58cc03bb
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-03-01 20:31:27 +0100

    Update obs-full-junest.sh

--- obs-full-junest.sh
+++ obs-full-junest.sh
@@ -114,7 +114,8 @@ if [ -n "$COMPILERS" ]; then
 	./.local/share/junest/bin/junest -- yay --noconfirm -S $COMPILERS
 fi
 if [ -n "$DEPENDENCES" ]; then
-	./.local/share/junest/bin/junest -- yay --noconfirm -S $DEPENDENCES
+	./.local/share/junest/bin/junest -- yay --noconfirm -R icu
+ 	./.local/share/junest/bin/junest -- yay --noconfirm -S $DEPENDENCES
 fi
 if [ -n "$APP" ]; then
 	./.local/share/junest/bin/junest -- yay --noconfirm -S alsa-lib gtk3 xapp
