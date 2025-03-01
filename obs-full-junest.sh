fd89c0532cb101b1e55ea27b49c8cb3c683d243d
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-03-01 20:05:59 +0100

    Update obs-full-junest.sh - add icu

--- obs-full-junest.sh
+++ obs-full-junest.sh
@@ -2,7 +2,7 @@
 
 APP=obs-studio-browser
 BIN="obs" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
-DEPENDENCES="python leancrypto" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
+DEPENDENCES="python leancrypto icu" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
 BASICSTUFF="binutils debugedit gzip"
 COMPILERS="base-devel"
 
