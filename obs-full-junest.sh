989282bb9de67b6eab64170afe8b444d41982c5d
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-03-16 22:40:50 +0100

    Update obs-full-junest.sh - compile using "asio-git"

--- obs-full-junest.sh
+++ obs-full-junest.sh
@@ -4,7 +4,7 @@ APP=obs-studio-browser
 BIN="obs" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
 DEPENDENCES="python leancrypto" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
 BASICSTUFF="binutils debugedit gzip"
-COMPILERS="base-devel"
+COMPILERS="base-devel asio-git"
 
 #############################################################################
 #	KEYWORDS TO FIND AND SAVE WHEN COMPILING THE APPIMAGE
