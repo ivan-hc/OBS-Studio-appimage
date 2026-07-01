e590c23d260c071647d018c48f71bb45011aa758
iVAN <88724353+ivan-hc@users.noreply.github.com>
2026-07-02 01:53:20 +0200

    Update obs-studio-all-junest.sh
    - add wayland dependency, ref https://github.com/ivan-hc/OBS-Studio-appimage/issues/34

--- obs-studio-all-junest.sh
+++ obs-studio-all-junest.sh
@@ -2,7 +2,7 @@
 
 APP=obs-studio-stable
 BIN="obs" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
-DEPENDENCES="libfdk-aac luajit pipiwire python" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
+DEPENDENCES="libfdk-aac luajit pipiwire python wayland" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
 BASICSTUFF="binutils debugedit gzip"
 COMPILERS="base-devel"
 
